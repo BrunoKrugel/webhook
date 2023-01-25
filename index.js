1a2a2e14f5593b045883bb5473e3641abde81305
Bruno Krugel <bruno.krugel@ifood.com.br>
2023-01-25 16:36:24 -0300

    add fallback for empty webhook

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ import updateProduct from "./src/dao.js";
 
 const app = express();
 
+const isObjectEmpty = (objectName) => {
+  for (let prop in objectName) {
+    if (objectName.hasOwnProperty(prop)) {
+      return false;
+    }
+  }
+  return true;
+};
+
 app.use(bodyParser.json());
 
 // use the express-static middleware
@@ -16,15 +25,29 @@ app.get("/", function (req, res) {
 });
 
 app.post("/hook", (req, res) => {
-  res.status(200).end(); // Responding is important
-  let data = {
-    product_id: req.body.Product.product_id,
-    store_id: req.body.store_id,
-    product_name: req.body.Product.product_name,
-    date: req.body.created_at.slice(0, 10),
-  };
+  
+  res.status(200).end();
+
+  let data = {};
+
+  if (req.body.hasOwnProperty("Product")) {
+    data.product_id = req.body.Product.product_id;
+    data.product_name = req.body.Product.product_name;
+  }
+  if (req.body.hasOwnProperty("store_id")) {
+    data.store_id = req.body.store_id;
+  }
+  if (req.body.hasOwnProperty("created_at")) {
+    data.date = req.body.created_at.slice(0, 10);
+  }
+
+  if (isObjectEmpty(data)) {
+    console.log("Empty object");
+    return;
+  }
+
   updateProduct(data);
-  //console.log(data); // Call your action on the request here
+  // console.log(data); // Call your action on the request here
 });
 
 // start the server listening for requests
