0fbb4f12395c25ccf6430a38b2ed4b6aaa984796
Bruno Krugel <brunokrugel@gmail.com>
2023-04-09 03:25:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,28 +25,27 @@ app.get("/", function (req, res) {
 });
 
 app.post("/hook", (req, res) => {
-  
   res.status(200).end();
 
   let data = {};
+  console.log(req.body);
+  // if (req.body.hasOwnProperty("Product")) {
+  //   data.product_id = req.body.Product.product_id;
+  //   data.product_name = req.body.Product.product_name;
+  // }
+  // if (req.body.hasOwnProperty("store_id")) {
+  //   data.store_id = req.body.store_id;
+  // }
+  // if (req.body.hasOwnProperty("created_at")) {
+  //   data.date = req.body.created_at.slice(0, 10);
+  // }
 
-  if (req.body.hasOwnProperty("Product")) {
-    data.product_id = req.body.Product.product_id;
-    data.product_name = req.body.Product.product_name;
-  }
-  if (req.body.hasOwnProperty("store_id")) {
-    data.store_id = req.body.store_id;
-  }
-  if (req.body.hasOwnProperty("created_at")) {
-    data.date = req.body.created_at.slice(0, 10);
-  }
+  // if (isObjectEmpty(data)) {
+  //   console.log("Empty object");
+  //   return;
+  // }
 
-  if (isObjectEmpty(data)) {
-    console.log("Empty object");
-    return;
-  }
-
-  updateProduct(data);
+  // updateProduct(data);
   // console.log(data); // Call your action on the request here
 });
 
